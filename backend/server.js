e60e44d54426cdac546c4474cf0f20724a836ed1
pranav-carnot-research <166536807+pranav-carnot-research@users.noreply.github.com>
2024-05-21 16:11:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-const PORT = 5000;
-const MONGO_URI = "mongodb+srv://user2:user2@cluster0.sfiyids.mongodb.net/";
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -13,7 +15,7 @@ app.use(cors());
 
 // MongoDB connection
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
